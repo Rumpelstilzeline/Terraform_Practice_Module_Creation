1306c40e9e7eecc3c63bc0381506fa591180c90c
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-11-03 13:08:04 +0100

    added a compute file to launch an EC2 instance to the project (with Ubuntu amd Jammy ami)

---
+++ 06-resources/compute.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web" {
+    # ubuntu amd64 Jammy ami
+  ami                         = "ami-0ef0fafba270833fc"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+
+  subnet_id = aws_subnet.public.id
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+
+}
