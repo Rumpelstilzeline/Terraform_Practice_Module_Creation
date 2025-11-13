3e30c83240093236957c639bb0b782b57c3962b3
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-11-13 10:43:39 +0100

    passing ec2 configuration as variables, including default values for type, instance type, volume size and type

--- 08-input-vars-locals-outputs/variables.tf
+++ 08-input-vars-locals-outputs/variables.tf
@@ -1,4 +1,28 @@
-variable "aws_region" {
+# variable "aws_region" {
+#   type = string
+# }
 
+
+variable "ec2_instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "size of newly managed ec2 instances"
+
+  validation {
+    condition     = var.ec2_instance_type == "t2.micro" || var.ec2_instance_type == "t3.micro"
+    error_message = "Only supports t2.micro and t3.micro"
+  }
 }
 
+variable "ec2_volume_config" {
+  type = object({
+    size = number
+    type = string
+  })
+  description = "The size and type of the root block volume for EC2 instances."
+
+  default = {
+    size = 10
+    type = "gp3"
+  }
+}
