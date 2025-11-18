b3c1b72066bf267b00d43ffdcb6c2c2d2183bd0e
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-11-18 21:51:17 +0100

    New folder: creating multiple resources. 1st step: creating a vpc and define the number of subnets with count. Also define cidr_block, name and tags of subnets  dynamically with count.index

---
+++ 11-multiple-resources/networking.tf
@@ -0,0 +1,25 @@
+locals {
+  project = "11-multiple-resources"
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/18"
+
+  tags = {
+    Project = local.project
+    Name    = local.project
+  }
+}
+
+resource "aws_subnet" "main" {
+  count      = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+
+  tags = {
+    Project = local.project
+    Name    = "${local.project}-${count.index}"
+  }
+}
+
