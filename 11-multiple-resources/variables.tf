6767e0613b2a132416591bb2a83a550995259695
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-11-20 14:10:32 +0100

    added validation for instance type and ami of ec2_instance_config_list variable

--- 11-multiple-resources/variables.tf
+++ 11-multiple-resources/variables.tf
@@ -13,4 +13,22 @@ variable "ec2_instance_config_list" {
     instance_type = string
     ami           = string
   }))
+
+  # Ensure that only t2.micro is used
+  # 1. Map from the object to the instance_type
+  # 2. Map from the instance_type to a boolean indicating whether the value equals t2.micro
+  # 3. Check wheter the list of booleans contains only true values
+
+  validation {
+    condition = alltrue([for config in var.ec2_instance_config_list : config.instance_type == "t2.micro"])
+    # Mapping: Jedes Element (config) in ec2_instance_config_list wird gemappt mit jedem Element (config) mit Key instance_type, wird in Liste geschrieben
+    error_message = "Only t2.micro instances are allowed"
+    }
+
+    validation {
+    condition = alltrue([for config in var.ec2_instance_config_list : config.ami == "ubuntu"])
+    # Mapping: Jedes Element (config) in ec2_instance_config_list wird gemappt mit jedem Element (config) mit Key ami, wird in Liste geschrieben
+    error_message = "Only ubuntu amis are allowed"
+    }
+  
 }
