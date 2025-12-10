215a1bd60a72acc71d00631a8f6db78f54e29b4a
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-12-10 14:19:20 +0100

    Created folder for user management. users and their role defined in user-roles.yaml. Content of .yaml-file were used in users.tf to create aws_iam_user ressources in AWS cloud

---
+++ proj02-iam-users/users.tf
@@ -0,0 +1,19 @@
+# yamldecode() => wandelt YAML-String in Terraform-Datensturktur um
+# file() => liest angegebene Datei als Test ein, Ergebnis ist ein YAML-String, der noch dekodiert werden muss
+
+# local.users_from_yaml.users => die Liste aus YAMLS
+# [*].username => splat expression, um alle Usernamen zu extrahieren
+# toset() => notwendig weil for_each ein Set/Map erwartet
+
+locals {
+  users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml"))
+}
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml.users[*].username)
+  name     = each.value
+}
+
+output "users" {
+  value = local.users_from_yaml.users
+}
