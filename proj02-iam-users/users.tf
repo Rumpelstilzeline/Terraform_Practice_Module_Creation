2647aa15cd9d7a138ce14a9778c5254352572741
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-12-10 14:52:54 +0100

    added Login for already created users with aws_iam_user_login_profile ressource, password will be shown in state file

--- proj02-iam-users/users.tf
+++ proj02-iam-users/users.tf
@@ -5,6 +5,9 @@
 # [*].username => splat expression, um alle Usernamen zu extrahieren
 # toset() => notwendig weil for_each ein Set/Map erwartet
 
+
+# ignose_changes notwendig, weil sich sonst jedes Mal ein neuer User erstellen würde, wenn man die Eigenschaften ändert
+
 locals {
   users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml"))
 }
@@ -12,6 +15,20 @@ locals {
 resource "aws_iam_user" "users" {
   for_each = toset(local.users_from_yaml.users[*].username)
   name     = each.value
+}
+
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
 }
 
 output "users" {
