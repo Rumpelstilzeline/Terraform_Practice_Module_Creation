ccb810e15b1949e8c83996651fa511bd3ca3b46c
Rumpelstilzeline <115560323+Rumpelstilzeline@users.noreply.github.com>
2025-12-12 11:43:25 +0100

    attaching policies to roles with arn of policy

--- proj02-iam-users/roles.tf
+++ proj02-iam-users/roles.tf
@@ -1,35 +1,59 @@
 locals {
   role_policies = {
     readonly = [
-        "ReadOnlyAccess"
+      "ReadOnlyAccess"
     ]
     admin = [
-        "AdminstratorAccess"
+      "AdminstratorAccess"
     ]
     auditor = [
-        "SecurityAudit"
+      "SecurityAudit"
     ]
     developer = [
-        "AmazonVPCFullAccess",
-        "AmazonEC2FullAccess",
-        "AmazonRDSFullAccess"
+      "AmazonVPCFullAccess",
+      "AmazonEC2FullAccess",
+      "AmazonRDSFullAccess"
     ]
   }
+
+  role_policies_list = flatten([
+    for role, policies in local.role_policies : [
+      for policy in policies : {
+        role   = role
+        policy = policy
+      }
+    ]
+  ])
+}
+
+output "policies" {
+  value = local.role_policies_list
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
-    actions = [ "sts:AssumeRole" ]
+    actions = ["sts:AssumeRole"]
 
     principals {
-      type = "AWS"
-      identifiers = [""]   
+      type        = "AWS"
+      identifiers = [""]
     }
   }
 }
 
 resource "aws_iam_role" "roles" {
-  for_each = toset(keys(local.role_policies))
-  name = each.key
+  for_each           = toset(keys(local.role_policies))
+  name               = each.key
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy" "managed_policies" {
+  for_each = toset(local.role_policies_list[*].policy)
+  arn      = "arn:aws:iam::aws:policy/${each.value}"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachments" {
+  count      = length(local.role_policies_list)
+  role       = aws_iam_role.roles[local.role_policies_list[count.index].role].name
+  policy_arn = data.aws_iam_policy.managed_policies[local.role_policies_list[count.index].role].arn
 }
